1e573b65fd50705e1ec8fea308817cd478ab731b
Anton Kuzmin <sassanid18@gmail.com>
2020-03-02 20:53:46 +0300

    Initial Commit

---
+++ Gittker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gittker
+//
+//  Created by uuttff8 on 3/2/20.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GittkerTests/GittkerTests.swift
@@ -0,0 +1,34 @@
+//
+//  GittkerTests.swift
+//  GittkerTests
+//
+//  Created by uuttff8 on 3/2/20.
+//  Copyright © 2020 Anton Kuzmin. All rights reserved.
+//
+
+import XCTest
+@testable import Gittker
+
+class GittkerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
