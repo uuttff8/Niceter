fc07c609065306c6373f2f1a5aafdc1a5d4aa64e
Anton Kuzmin <sassanid18@gmail.com>
2020-03-12 20:45:07 +0300

    init search

--- Gittker/Features/Search/SearchViewController.swift
+++ Gittker/Features/Search/SearchViewController.swift
@@ -12,21 +12,34 @@ class SearchViewController: UIViewController, Storyboarded {
     
     weak var coordinator: SearchCoordinator?
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+
+}
+
+// MARK: - Searching
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // If the search bar contains text, filter our data with the string
+        if let searchText = searchController.searchBar.text {
+//            filterContent(for: searchText)
+            // Reload the table view with the search result data.
+            tableView.reloadData()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    func filterContent(for searchText: String) {
+//        // Update the searchResults array with matches
+//        // in our entries based on the title value.
+//        searchResults = entries.filter({ (title: String, image: String) -> Bool in
+//            let match = title.range(of: searchText, options: .caseInsensitive)
+//            // Return the tuple if the range contains a match.
+//            return match != nil
+//        })
+//    }
 }
