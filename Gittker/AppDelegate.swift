67e73656914b1067bd5a138a9d521e844c4495ee
Anton Kuzmin <sassanid18@gmail.com>
2020-03-19 17:49:09 +0300

    refactor app coordinator

--- Gittker/AppDelegate.swift
+++ Gittker/AppDelegate.swift
@@ -14,23 +14,26 @@ import KeychainSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    var coordinator: AppCoordinator?
+    let appCoordinator = AppCoordinator()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let navController = UINavigationController()
+        appCoordinator.window.makeKeyAndVisible()
 
-        // send that into our coordinator so that it can display view controllers
-        coordinator = AppCoordinator(navigationController: navController)
-
-        // tell the coordinator to take over control
-        coordinator?.start()
-
-        // create a basic UIWindow and activate it
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navController
-        window?.makeKeyAndVisible()
         
+//        let navController = UINavigationController()
+
+//        // send that into our coordinator so that it can display view controllers
+//        coordinator = AppCoordinator(navigationController: navController)
+//
+//        // tell the coordinator to take over control
+//        coordinator?.start()
+//
+//        // create a basic UIWindow and activate it
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = navController
+//        window?.makeKeyAndVisible()
+//
         return true
     }
 
