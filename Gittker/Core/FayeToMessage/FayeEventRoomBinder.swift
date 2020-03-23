e1e5eb1fb1612b92ded11de7df700063c6dca3e4
Anton Kuzmin <sassanid18@gmail.com>
2020-03-23 17:19:25 +0300

    init

--- Gittker/Core/FayeToMessage/FayeEventRoomBinder.swift
+++ Gittker/Core/FayeToMessage/FayeEventRoomBinder.swift
@@ -16,7 +16,19 @@ extension UIImage {
         
         self.init(data: imageData)
     }
-    
+}
+
+extension UIImage {
+
+    func decodedImage() -> UIImage {
+        guard let cgImage = cgImage else { return self }
+        let size = CGSize(width: cgImage.width, height: cgImage.height)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: cgImage.bytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        context?.draw(cgImage, in: CGRect(origin: .zero, size: size))
+        guard let decodedImage = context?.makeImage() else { return self }
+        return UIImage(cgImage: decodedImage)
+    }
 }
 
 class FayeEventRoomBinder: NSObject {
