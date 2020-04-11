f2e2901505f1d7476a91183aa5fae48c986c5b4f
Anton Kuzmin <sassanid18@gmail.com>
2020-04-11 14:45:27 +0300

    fixing bugs

--- Gittker/Utils/Extensions/Extensions.swift
+++ Gittker/Utils/Extensions/Extensions.swift
@@ -31,3 +31,14 @@ extension UIView {
         layer.mask = mask
     }
 }
+
+extension CATransaction {
+
+    static func disableAnimations(_ completion: () -> Void) {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        completion()
+        CATransaction.commit()
+    }
+
+}
